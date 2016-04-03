a7c01a1300e6f6a5559aa1092486f9550791bd11
Trey Hoover <treyhoover@gmail.com>
2016-04-03 12:45:15 -0400

    Update gitignore

--- components/App.js
+++ components/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import TodoList from '../Pages/TodoList';
-import Counter from '../Pages/Counter';
-import Home from '../Pages/Home';
-import NoMatch from '../Pages/NoMatch';
+import TodoList from '../pages/TodoList';
+import Counter from '../pages/Counter';
+import Home from '../pages/Home';
+import NoMatch from '../pages/NoMatch';
 import { Router, Route, Link, browserHistory } from 'react-router'
 
 const App = () => (
